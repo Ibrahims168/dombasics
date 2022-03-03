877b0dec46c58ef8b77fa31c96155a879a9ca7ac
ibrahims168 <ibrahh881@gmail.com>
2022-03-03 17:01:13 +0200

    adding input

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ const uniqueHeaders = document.getElementsByClassName('unique')
 const header = document.querySelector('#superUnique')
 const body = document.getElementsByTagName('body')[0]
 const dynamicElem = document.createElement('div')
+const input = document.querySelector('#myInput')
+const btn = document.getElementById('myBtn')
+
+
+btn.addEventListener('click', function () {
+    const text = input.value
+    const post = document.createElement('h1')
+    post.innerText = text
+    body.append(post)
+})
+
+
+
 dynamicElem.id = 'myDynamicElem'
 dynamicElem.classList.add('dynamic')
 body.append(dynamicElem)
+
